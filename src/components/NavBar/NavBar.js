e643d331b7f484edd8de8603745eb924ea008ded
Samuel Trahan <samtrahan91@gmail.com>
2020-10-18 19:33:28 -0500

    nav bar add

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="nav-bar">
+        <a className="link"a href='/'>IDK</a>
+      </nav>
+    </div>
+  )
+}
