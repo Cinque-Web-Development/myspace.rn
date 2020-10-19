90e340755bf3ee55d1e8c8217d01888016b9462c
Samuel Trahan <samtrahan91@gmail.com>
2020-10-18 20:20:54 -0500

    nav bar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,15 @@ import './NavBar.css'
 
 export default function NavBar() {
   return (
-    <div>
-      <nav className="nav-bar">
-        <a className="link"a href='/'>IDK</a>
+    <div className="nav-bar">
+      <div>
+      <nav>
+        <ul>
+        <li><a className="logo" href="/"><img className="myspace-logo" alt="" src="./images/myspace.png"></img></a></li>
+        <li><a className="link"a href='/'>IDK</a></li>
+        </ul>
       </nav>
+      </div>
     </div>
   )
 }
