8e52e310d71780f7db19d26d84cc125636e49043
Samuel Trahan <samtrahan91@gmail.com>
2020-11-02 15:13:01 -0600

    packages fix

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from '../components/NavBar/NavBar';
 
 import './App.css';
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
-import "./NavBar.css";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './NavBar.css';
 
 export default function NavBar() {
   const history = useHistory();
   return (
     <div className="nav-bar">
       <nav>
-      <div
-            onKeyPress={event => {
-              if (event.key === 'Enter') {
-                history.push('/');
-              }
-            }}
-            onClick={() => {
+        <div
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
               history.push('/');
-            }}
-            role="button"
-            tabIndex="0"
-          >
-            <img className="logo" alt="" src="/images/myspace-lo.png" />
-            <span>OurSpace</span>
-          </div>
+            }
+          }}
+          onClick={() => {
+            history.push('/');
+          }}
+          role="button"
+          tabIndex="0"
+        >
+          <img className="logo" alt="" src="/images/myspace-lo.png" />
+          <span>OurSpace</span>
+        </div>
       </nav>
     </div>
   );
