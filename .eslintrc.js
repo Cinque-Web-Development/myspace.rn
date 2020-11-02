00ce883a4531b62e3b14ec4c4d41284fe74a63d0
Kathleen Stickel <kstick9210@outlook.com>
2020-11-02 16:59:40 -0600

    trying to fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,50 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-    },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'airbnb/hooks',
-        'eslint-config-prettier',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: ['react', 'eslint-plugin-prettier'],
-    rules: {
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                js: 'never',
-                jsx: 'never',
-            },
-        ],
-        'import/no-extraneous-dependencies': [0, false],
-        'import/prefer-default-export': [0, false],
-        'prettier/prettier': 'error',
-        'react/jsx-filename-extension': [0, false],
-        'react/jsx-one-expression-per-line': [0, false],
-        'react/prop-types': [0, false],
-        'react/react-in-jsx-scope': [0, false],
-        'react-hooks/exhaustive-deps': 'warn',
-        'react/jsx-indent': [2, 'tab'],
-    },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx'],
-            },
-        },
-    },
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb/hooks',
+		'eslint-config-prettier',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'eslint-plugin-prettier'],
+	rules: {
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+			},
+		],
+		'import/no-extraneous-dependencies': [0, false],
+		'import/prefer-default-export': [0, false],
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [0, false],
+		'react/jsx-one-expression-per-line': [0, false],
+		'react/prop-types': [0, false],
+		'react/react-in-jsx-scope': [0, false],
+		'react-hooks/exhaustive-deps': 'warn',
+		'react/jsx-indent': [4, 'tab'],
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx'],
+			},
+		},
+	},
 };
--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -3,10 +3,9 @@ import React from 'react';
 import './UserPage.css';
 
 export default function UserPage() {
-    return (
-        <div className="UserPage">
-            <div className="left-col"></div>
-            <div className="right-col"></div>
-        </div>
-    );
+	return (
+		<div>
+			<div>User PAge</div>
+		</div>
+	);
 }
