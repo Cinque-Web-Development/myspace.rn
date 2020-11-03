8d4245ecebbd0d0c1533a684ed1e20ae3bbf3f12
Kathleen Stickel <kstick9210@outlook.com>
2020-11-03 09:17:08 -0600

    style user info section

--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './UserPage.css';
+import profilePhoto from '../../Assets/profile-photo-placeholder.png';
 
 export default function UserPage() {
 	return (
@@ -8,25 +9,27 @@ export default function UserPage() {
 			<div className="left-col">
 				<div className="user-info">
 					<div className="user-info-left">
-						<p>Kathleen</p>
-						<img></img>
+						<h2>Kathleen</h2>
+						<img src={profilePhoto} className="profile-photo"></img>
 					</div>
 					<div className="user-info-right">
-						<p>headline</p>
+						<p>I'm the React Queen</p>
 						<p>Female</p>
 						<p>28 years old</p>
 						<p>Dallas, TX, United States</p>
 						<p>Last Login: 11/2/2020</p>
 					</div>
 				</div>
-				<div className="contact-links"></div>
+				<div className="contact-links">
+					<h3>Contacting Kathleen</h3>
+				</div>
 				<div className="profile-song"></div>
 			</div>
 			<div className="right-col">
-				<div className="user-about">
+				<div>
 					<h3>Kathleen's Blurbs</h3>
-					<h4>About me:</h4>
-					<p>I'm the React Queen</p>
+					<h4 className="user-about-heading">About me:</h4>
+					<p className="user-about">I'm the React Queen</p>
 				</div>
 				<div className="user-friends"></div>
 			</div>
