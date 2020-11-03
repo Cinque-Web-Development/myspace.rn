9f74a6394e83c30037865c59aa0e5c0d691bdd19
Samuel Trahan <samtrahan91@gmail.com>
2020-11-03 11:03:40 -0600

    user page link add

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 export default function NavBar() {
-  const history = useHistory();
-  return (
-    <div className="nav-bar">
-      <nav>
-        <div
-          onKeyPress={event => {
-            if (event.key === 'Enter') {
-              history.push('/');
-            }
-          }}
-          onClick={() => {
-            history.push('/');
-          }}
-          role="button"
-          tabIndex="0"
-        >
-          <img className="logo" alt="" src="/images/myspace-lo.png" />
-          <span>OurSpace</span>
-        </div>
-      </nav>
-    </div>
-  );
+	const history = useHistory();
+	return (
+		<nav className="nav-bar">
+			<div
+				className="logo-link"
+				onKeyPress={event => {
+					if (event.key === 'Enter') {
+						history.push('/');
+					}
+				}}
+				onClick={() => {
+					history.push('/');
+				}}
+				role="button"
+				tabIndex="0"
+			>
+				<img className="logo" alt="" src="/images/myspace-lo.png" />
+				<span>OurSpace</span>
+			</div>
+			<div className="link-nav">
+				<Link className="user-page" to="/user">
+					User Page
+				</Link>
+			</div>
+		</nav>
+	);
 }
