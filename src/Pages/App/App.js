a2b2fb32a9b3e28327730449812b2353f2fe9c37
Kathleen Stickel <kstick9210@outlook.com>
2020-11-05 15:39:03 -0600

    import services and update NavBar

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -4,16 +4,52 @@ import NavBar from '../../components/NavBar/NavBar';
 import LandingPage from '../LandingPage/LandingPage';
 import Footer from '../../components/Footer/Footer';
 import UserPage from '../UserPage/UserPage';
+import LoginPage from '../LoginPage/LoginPage';
+import SignupPage from '../SignupPage/SignupPage';
+
+import userService from '../../services/userService';
 
 import './App.css';
 
 export default function App() {
+	const [user, setUser] = useState('');
+
+	const handleLogout = () => {
+		userService.logout();
+		setUser(null);
+	};
+
+	const handleSignupOrLogin = () => {
+		setUser(userService.getUser());
+	};
+
 	return (
 		<>
 			<Router>
-				<NavBar />
+				<NavBar user={user} handleLogout={handleLogout} />
 				<Route exact path="/" render={() => <LandingPage />} />
 				<Route exact path="/user" render={() => <UserPage />} />
+				<Route
+					exact
+					path="/login"
+					render={({ history }) => (
+						<LoginPage
+							history={history}
+							handleSignupOrLogin={handleSignupOrLogin}
+						/>
+					)}
+				/>
+
+				<Route
+					exact
+					path="/signup"
+					render={({ history }) => (
+						<SignupPage
+							history={history}
+							handleSignupOrLogin={handleSignupOrLogin}
+						/>
+					)}
+				/>
 				<Footer />
 			</Router>
 		</>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-export default function NavBar() {
+export default function NavBar({ user, handleLogout }) {
 	const history = useHistory();
 	return (
 		<nav className="nav-bar">
@@ -23,11 +23,31 @@ export default function NavBar() {
 				<img className="logo" alt="" src="/images/myspace-lo.png" />
 				<span>OurSpace</span>
 			</div>
-			<div className="link-nav">
-				<Link className="user-page" to="/user">
-					User Page
-				</Link>
-			</div>
+
+			{user ? (
+				<div className="nav-wrapper">
+					<Link to={`/user/${user._id}`} className="nav-link">
+						Welcome, {user.name}
+					</Link>
+					<Link to=" " onClick={handleLogout} className="nav-link">
+						Log Out
+					</Link>
+					<div className="link-nav">
+						<Link className="user-page" to="/user">
+							User Page
+						</Link>
+					</div>
+				</div>
+			) : (
+				<div className="nav-wrapper">
+					<Link to="/login" className="nav-link">
+						Log In
+					</Link>
+					<Link to="/signup" className="nav-link">
+						Sign Up
+					</Link>
+				</div>
+			)}
 		</nav>
 	);
 }
